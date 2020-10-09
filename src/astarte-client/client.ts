9175c1c7b242e9f2344ac9edf6c81bf504f08722
Davide Briani <davide.briani@ispirata.com>
2020-10-09 12:46:45 +0200

    Add Typescript's types to Group Devices page
    Move Group Devices page to typescript
    
    Signed-off-by: Davide Briani <davide.briani@ispirata.com>

--- src/astarte-client/client.ts
+++ src/astarte-client/client.ts
@@ -346,17 +346,14 @@ class AstarteClient {
     return response.data.map((device: AstarteDeviceDTO) => AstarteDevice.fromObject(device));
   }
 
-  async removeDeviceFromGroup(params: any): Promise<void> {
+  async removeDeviceFromGroup(params: { groupName: string; deviceId: string }): Promise<void> {
     const { groupName, deviceId } = params;
-
     if (!groupName) {
       throw Error('Invalid group name');
     }
-
     if (!deviceId) {
       throw Error('Invalid device ID');
     }
-
     await this.$delete(
       this.apiConfig.deviceInGroup({
         ...this.config,
